fb68f83e33f68db0e173f1e2898784b9853f30df
adilabdu <adilabdu68@gmail.com>
2022-04-26 22:28:23 +0900

    Navigation and Add Icon added

--- src/renderer/components/Navigation.vue
+++ src/renderer/components/Navigation.vue
@@ -15,86 +15,109 @@
       </div>
 
       <ul id="quickLinks" class="relative flex flex-col gap-[2px] text-[15px] items-start text-[#EFEFEF] w-full my-[16px]">
-        <Router to="/listen" class="h-[32px] rounded-md w-full py-[1px] flex justify-start items-center gap-4 px-2 transition duration-300"
+        <RouterLink to="/listen" class="h-[32px] rounded-md w-full py-[1px] flex justify-start items-center gap-4 px-2 transition duration-300"
             :class="active === '/listen' ? 'bg-[#3F3F41]' : ''">
           <PlayRoundIcon :width="16" :class="'fill-[#FF8400]'" />
           <p class="truncate">Listen Now</p>
-        </Router>
-        <NuxtLink to="/browse" class="h-[32px] rounded-md w-full py-[1px] flex justify-start items-center gap-4 px-2 transition duration-300"
+        </RouterLink>
+        <RouterLink to="/browse" class="h-[32px] rounded-md w-full py-[1px] flex justify-start items-center gap-4 px-2 transition duration-300"
             :class="active === '/browse' ? 'bg-[#3F3F41]' : ''">
           <BrowseIcon :stroke="{ width: 18, color: '#FF8400' }" :width="16" />
           <p class="truncate">Browse</p>
-        </NuxtLink>
-        <NuxtLink to="radio" class="h-[32px] rounded-md w-full py-[1px] flex justify-start items-center gap-4 px-2 transition duration-300"
+        </RouterLink>
+        <RouterLink to="/radio" class="h-[32px] rounded-md w-full py-[1px] flex justify-start items-center gap-4 px-2 transition duration-300"
             :class="active === '/radio' ? 'bg-[#3F3F41]' : ''">
           <RadioIcon :class="'fill-[#FF8400]'" :width="16" />
           <p class="truncate">Radio</p>
-        </NuxtLink>
+        </RouterLink>
       </ul>
 
-      <h3 class="text-[10px] text-[#FFFFFFA3] font-medium w-full ml-[1px]">Library</h3>
-      <ul id="libraryLinks" class="relative flex flex-col gap-[2px] text-[15px] items-start text-[#EFEFEF] w-full mb-[16px]">
-        <NuxtLink to="/recently-added" class="h-[32px] rounded-md w-full py-[1px] flex justify-start items-center gap-4 px-2"
-            :class="active === '/recently-added' ? 'bg-[#3F3F41]' : ''">
-          <RecentIcon :width="16" :class="'fill-[#FF8400]'" />
-          <p class="truncate">Recently Added</p>
-        </NuxtLink>
-        <NuxtLink to="/artists" class="h-[32px] rounded-md w-full py-[1px] flex justify-start items-center gap-4 px-2"
-            :class="active === '/artists' ? 'bg-[#3F3F41]' : ''">
-          <ArtistIcon :stroke="{ width: 18, color: '#FF8400' }" :width="16" :class="'fill-[#FF8400]'" />
-          <p class="truncate">Artists</p>
-        </NuxtLink>
-        <NuxtLink to="/albums" class="h-[32px] rounded-md w-full py-[1px] flex justify-start items-center gap-4 px-2"
-            :class="active === '/albums' ? 'bg-[#3F3F41]' : ''">
-          <AlbumIcon :stroke="{ width: 18, color: '#FF8400' }" :width="16" :class="'fill-[#272728]'" />
-          <p class="truncate">Albums</p>
-        </NuxtLink>
-        <NuxtLink to="/songs" class="h-[32px] rounded-md w-full py-[1px] flex justify-start items-center gap-4 px-2"
-            :class="active === '/songs' ? 'bg-[#3F3F41]' : ''">
-          <SongIcon :class="'fill-[#FF8400] mr-[0.375rem]'" :width="10" />
-          <p class="truncate">Songs</p>
-        </NuxtLink>
-      </ul>
+      <div class="w-full group font-light">
+        <div class="flex justify-between items-start w-full">
+          <h3 class="text-[10px] text-[#FFFFFFA3] font-medium ml-[1px]">Library</h3>
+          <h3 class="text-[12px] text-[#FFFFFFA3] font-medium ml-[1px] group-hover:opacity-100 opacity-0">+</h3>
+        </div>
+        <ul id="libraryLinks" class="relative flex flex-col gap-[2px] text-[15px] items-start text-[#EFEFEF] w-full mb-[16px]">
+          <RouterLink to="/recently-added" class="h-[32px] rounded-md w-full py-[1px] flex justify-start items-center gap-4 px-2"
+                      :class="active === '/recently-added' ? 'bg-[#3F3F41]' : ''">
+            <RecentIcon :width="16" :class="'fill-[#FF8400]'" />
+            <p class="truncate">Recently Added</p>
+          </RouterLink>
+          <RouterLink to="/artists" class="h-[32px] rounded-md w-full py-[1px] flex justify-start items-center gap-4 px-2"
+                      :class="active === '/artists' ? 'bg-[#3F3F41]' : ''">
+            <ArtistIcon :stroke="{ width: 18, color: '#FF8400' }" :width="16" :class="'fill-[#FF8400]'" />
+            <p class="truncate">Artists</p>
+          </RouterLink>
+          <RouterLink to="/albums" class="h-[32px] rounded-md w-full py-[1px] flex justify-start items-center gap-4 px-2"
+                      :class="active === '/albums' ? 'bg-[#3F3F41]' : ''">
+            <AlbumIcon :stroke="{ width: 18, color: '#FF8400' }" :width="16" :class="'fill-[#272728]'" />
+            <p class="truncate">Albums</p>
+          </RouterLink>
+          <RouterLink to="/songs" class="h-[32px] rounded-md w-full py-[1px] flex justify-start items-center gap-4 px-2"
+                      :class="active === '/songs' ? 'bg-[#3F3F41]' : ''">
+            <SongIcon :class="'fill-[#FF8400] mr-[0.375rem]'" :width="10" />
+            <p class="truncate">Songs</p>
+          </RouterLink>
+        </ul>
+      </div>
 
-      <h3 class="text-[10px] text-[#FFFFFFA3] font-medium w-full ml-[1px]">Playlists</h3>
-      <ul id="playlists" class="relative flex flex-col gap-[2px] text-[15px] items-start text-[#EFEFEF] w-full mb-[16px]">
-        <li class="h-[32px] rounded-md w-full py-[1px] flex justify-start items-center gap-4 px-2">
-          <PlaylistIcon :width="16" :class="'fill-[#B9B9B9]'" />
-          <a href="#" class="truncate">♾ playlist</a>
-        </li>
-        <li class="h-[32px] rounded-md w-full py-[1px] flex justify-start items-center gap-4 px-2">
-          <PlaylistIcon :width="16" :class="'fill-[#B9B9B9]'" />
-          <a href="#" class="truncate">B TEAM 🪑🔥</a>
-        </li>
-        <li class="h-[32px] rounded-md w-full py-[1px] flex justify-start items-center gap-4 px-2">
-          <PlaylistIcon :width="16" :class="'fill-[#B9B9B9]'" />
-          <a href="#" class="truncate">🤖 Siri Name That Tune!</a>
-        </li>
-        <li class="h-[32px] rounded-md w-full py-[1px] flex justify-start items-center gap-4 px-2">
-          <PlaylistIcon :width="16" :class="'fill-[#B9B9B9]'" />
-          <a href="#" class="truncate">contexts 📚</a>
-        </li>
-        <li class="h-[32px] rounded-md w-full py-[1px] flex justify-start items-center gap-4 px-2">
-          <PlaylistIcon :width="16" :class="'fill-[#B9B9B9]'" />
-          <a href="#" class="truncate">endgame season 🕛</a>
-        </li>
-        <li class="h-[32px] rounded-md w-full py-[1px] flex justify-start items-center gap-4 px-2">
-          <PlaylistIcon :width="16" :class="'fill-[#B9B9B9]'" />
-          <a href="#" class="truncate">ሊዩ ሊዩ</a>
-        </li>
-        <li class="h-[32px] rounded-md w-full py-[1px] flex justify-start items-center gap-4 px-2">
-          <PlaylistIcon :width="16" :class="'fill-[#B9B9B9]'" />
-          <a href="#" class="truncate">seasons: vol.1</a>
-        </li>
-        <li class="h-[32px] rounded-md w-full py-[1px] flex justify-start items-center gap-4 px-2">
-          <PlaylistIcon :width="16" :class="'fill-[#B9B9B9]'" />
-          <a href="#" class="truncate">sokcho vibes 🏖</a>
-        </li>
-        <li class="h-[32px] rounded-md w-full py-[1px] flex justify-start items-center gap-4 px-2">
-          <PlaylistIcon :width="16" :class="'fill-[#B9B9B9]'" />
-          <a href="#" class="truncate">2022 feels 🎇</a>
-        </li>
-      </ul>
+      <div class="w-full group font-light">
+        <div class="flex justify-between w-full items-center">
+          <h3 class="text-[10px] text-[#FFFFFFA3] font-medium ml-[1px]">Playlists</h3>
+          <h3 @click="createPlaylist" class="cursor-pointer text-[12px] text-[#FFFFFFA3] font-medium ml-[1px] opacity-0 group-hover:opacity-100">+</h3>
+        </div>
+        <ul id="playlists" class="relative flex flex-col gap-[2px] text-[15px] items-start text-[#EFEFEF] w-full mb-[16px]">
+          <li class="h-[32px] rounded-md w-full py-[1px] flex justify-start items-center gap-4 px-2">
+            <PlaylistIcon :width="16" :class="'fill-[#B9B9B9]'" />
+            <a href="#" class="truncate">♾ playlist</a>
+          </li>
+          <li class="h-[32px] rounded-md w-full py-[1px] flex justify-start items-center gap-4 px-2">
+            <PlaylistIcon :width="16" :class="'fill-[#B9B9B9]'" />
+            <a href="#" class="truncate">B TEAM 🪑🔥</a>
+          </li>
+          <li class="h-[32px] rounded-md w-full py-[1px] flex justify-start items-center gap-4 px-2">
+            <PlaylistIcon :width="16" :class="'fill-[#B9B9B9]'" />
+            <a href="#" class="truncate">🤖 Siri Name That Tune!</a>
+          </li>
+          <li class="h-[32px] rounded-md w-full py-[1px] flex justify-start items-center gap-4 px-2">
+            <PlaylistIcon :width="16" :class="'fill-[#B9B9B9]'" />
+            <a href="#" class="truncate">contexts 📚</a>
+          </li>
+          <li class="h-[32px] rounded-md w-full py-[1px] flex justify-start items-center gap-4 px-2">
+            <PlaylistIcon :width="16" :class="'fill-[#B9B9B9]'" />
+            <a href="#" class="truncate">endgame season 🕛</a>
+          </li>
+          <li class="h-[32px] rounded-md w-full py-[1px] flex justify-start items-center gap-4 px-2">
+            <PlaylistIcon :width="16" :class="'fill-[#B9B9B9]'" />
+            <a href="#" class="truncate">ሊዩ ሊዩ</a>
+          </li>
+          <li class="h-[32px] rounded-md w-full py-[1px] flex justify-start items-center gap-4 px-2">
+            <PlaylistIcon :width="16" :class="'fill-[#B9B9B9]'" />
+            <a href="#" class="truncate">seasons: vol.1</a>
+          </li>
+          <li class="h-[32px] rounded-md w-full py-[1px] flex justify-start items-center gap-4 px-2">
+            <PlaylistIcon :width="16" :class="'fill-[#B9B9B9]'" />
+            <a href="#" class="truncate">sokcho vibes 🏖</a>
+          </li>
+          <li class="h-[32px] rounded-md w-full py-[1px] flex justify-start items-center gap-4 px-2">
+            <PlaylistIcon :width="16" :class="'fill-[#B9B9B9]'" />
+            <a href="#" class="truncate">2022 feels 🎇</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="w-full group font-light">
+        <div class="flex justify-between w-full items-center">
+          <h3 class="text-[10px] text-[#FFFFFFA3] font-medium ml-[1px]">Playlists</h3>
+          <h3 @click="createPlaylist" class="cursor-pointer text-[12px] text-[#FFFFFFA3] font-medium ml-[1px] opacity-0 group-hover:opacity-100">+</h3>
+        </div>
+        <ul v-for="playlist in userPlaylists" id="dynamicPlaylists" class="relative flex flex-col gap-[2px] text-[15px] items-start text-[#EFEFEF] w-full mb-[16px]">
+          <li class="h-[32px] rounded-md w-full py-[1px] flex justify-start items-center gap-4 px-2">
+            <PlaylistIcon :width="16" :class="'fill-[#B9B9B9]'" />
+            <a href="#" class="truncate">{{ playlist.name }}</a>
+          </li>
+        </ul>
+      </div>
 
     </nav>
 
@@ -105,8 +128,12 @@
 
 <script setup>
 
-  import { computed } from "vue"
-  // import { useRoute } from "nuxt/app"
+  import { computed, watch, onMounted, ref } from "vue"
+  import { useRouter, useRoute } from 'vue-router'
+  import route from '../router/index'
+  import store from '../store'
+
+  import Playlist from '../models/playlist'
 
   import logo from "../static/favicon.png"
   import SearchIcon from "./Icons/search.vue"
@@ -119,8 +146,17 @@
   import SongIcon from "./Icons/song.vue"
   import PlaylistIcon from "./Icons/playlist.vue";
 
-  // const route = useRoute()
-  // const active = computed(() => route.path)
+  const active = computed(() => route.currentRoute.value.path)
+
+  function createPlaylist() {
+    console.log('1. User initiates New Playlist creation')
+    store.dispatch('createPlaylist', new Playlist('Orange! 🍊', 'Orange Music synthesis 2022'))
+  }
+
+  const userPlaylists = ref([])
+  onMounted(() => {
+    userPlaylists.value = store.state.playlists.allPlaylists
+  })
 
 </script>
 
