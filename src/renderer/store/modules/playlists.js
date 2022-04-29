269e7c1e02b2dc28aeae9461db59af00fb94125d
adilabdu <adilabdu68@gmail.com>
2022-04-29 23:19:47 +0300

    Removed console logs

--- src/renderer/store/modules/playlists.js
+++ src/renderer/store/modules/playlists.js
@@ -1,4 +1,3 @@
-import { useStorage } from "@vueuse/core";
 
 const state = {
     allPlaylists: localStorage.getItem('allPlaylists') || []
@@ -6,7 +5,6 @@ const state = {
 
 const actions = {
     createPlaylist(context, payload) {
-        console.log('3. Inside Vuex action: new Playlist object passed as payload: ', payload)
         context.commit('createPlaylist', payload)
     }
 }
@@ -15,7 +13,6 @@ const mutations = {
     createPlaylist(state, payload) {
         state.allPlaylists.push(payload)
         localStorage.setItem('allPlaylists', state.allPlaylists)
-        console.log('4. Inside Vuex mutation: new Playlist object pushed to list of playlists: ', state.allPlaylists)
     }
 }
 
