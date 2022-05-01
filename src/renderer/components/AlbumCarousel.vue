9c2a24192010e941cf8c9f0891cef9265cc6bbb3
adilabdu <adilabdu68@gmail.com>
2022-05-02 00:00:43 +0300

    Previous and Next buttons to paginate the tracks in the carousel

--- src/renderer/components/AlbumCarousel.vue
+++ src/renderer/components/AlbumCarousel.vue
@@ -1,20 +1,27 @@
 <template>
 
-  <section class="flex flex-col gap-[15px] my-[30px] min-w-0">
+  <section class="flex flex-col gap-[15px] my-[30px] min-w-0 group">
 
     <div class="flex justify-between items-center w-full px-[40px]">
       <h2 class="text-[17px] font-semibold">{{ title }}</h2>
       <a href="#" class="text-[15px] hover:underline text-[#FF8400]">See All</a>
     </div>
 
-    <div id="albumCarousel" class="flex flex-row flex-nowrap lg:px-[40px] lg:grid lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-[7.5px] lg:gap-[15px] lg:!w-full min-w-0 overflow-x-auto overflow-y-hidden"
+    <div id="albumCarousel" class="relative flex flex-row flex-nowrap lg:px-[40px] lg:grid lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-[7.5px] lg:gap-[15px] lg:!w-full min-w-0 overflow-x-auto overflow-y-hidden"
          :style="{ width: carouselWidth + 'px' }">
+
+      <div v-if="available.prev" @click="previous" class="overflow-hidden paginator flex items-center justify-center absolute z-10 w-0 overflow-hidden lg:w-[40px] h-full cursor-pointer transition duration-300">
+        <PreviousIcon class="paginator-button" :class="'group-hover:opacity-100 opacity-0 transition duration-300'" :width="8" :stroke="{width: 30, color: 'white'}" />
+      </div>
       <div class="min-w-[25px] lg:hidden">&nbsp;</div>
 
         <LoadingCard v-for="i in display" v-if="loading" />
 
         <slot v-else />
 
+      <div v-if="available.next" @click="next" class="overflow-hidden paginator flex items-center justify-center absolute right-0 z-10 w-0 overflow-hidden lg:w-[40px] h-full cursor-pointer transition duration-300">
+        <NextIcon class="paginator-button" :class="'group-hover:opacity-100 opacity-0 transition duration-300'" :width="8" :stroke="{width: 30, color: 'white'}" />
+      </div>
       <div class="min-w-[25px] lg:hidden">&nbsp;</div>
     </div>
 
@@ -28,12 +35,17 @@
   import { useWindowSize } from "@vueuse/core"
 
   import LoadingCard from "./LoadingCard.vue";
+  import NextIcon from "./Icons/next.vue"
+  import PreviousIcon from "./Icons/prev.vue"
 
   export default {
     name: "AlbumCarousel",
     components: {
-      LoadingCard
+      LoadingCard,
+      NextIcon,
+      PreviousIcon
     },
+    emits: ['next', 'prev'],
     props: {
       title: {
         required: true,
@@ -44,9 +56,16 @@
       },
       display: {
         type: Number
+      },
+      available: {
+        type: Object,
+        default: {
+          'prev': false,
+          'next': false
+        }
       }
     },
-    setup() {
+    setup(props, { emit }) {
 
       onMounted(() => {
         setCarouselWidth()
@@ -62,11 +81,19 @@
         }
       }
 
+      function previous() {
+        emit('previous')
+      }
+
+      function next() {
+        emit('next')
+      }
+
       watch(width, () => {
         setCarouselWidth()
       })
 
-      return { carouselWidth }
+      return { carouselWidth, previous, next }
 
     }
   }
@@ -82,6 +109,10 @@
   #albumCarousel::-webkit-scrollbar { /* WebKit */
     width: 0;
     height: 0;
+  }
+
+  .paginator:hover .paginator-button {
+    @apply stroke-white stroke-[60] scale-125;
   }
 
 </style>
