f559c5447806a7122fde45264add17fae9468a7c
adilabdu <adilabdu68@gmail.com>
2022-05-02 00:03:03 +0300

    Additional DeezerPlaylist added

--- src/renderer/pages/listen.vue
+++ src/renderer/pages/listen.vue
@@ -5,6 +5,7 @@
     <DeezerPlaylist :playlistID="'658600625'" :title="'New Releases'" />
     <DeezerPlaylist :playlistID="'7662551722'" :title="'2010\'s Raps'" />
     <DeezerPlaylist :playlistID="'1677006641'" :title="'Hip-Hop Hits'" />
+    <DeezerPlaylist :playlistID="'2098157264'" :title="'Hits of the Moment'" />
 
   </ContentPage>
 
