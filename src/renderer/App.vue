09f3f6063c002e5d646de9670930f7780ea16229
adilabdu <adilabdu68@gmail.com>
2022-05-01 14:45:58 +0300

    Example use of window.io contextBridge api

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,10 +1,11 @@
-<script setup lang="ts">
+<script setup>
 
 import Layout from './layouts/default.vue'
 
 import { ipcRenderer } from './electron'
 
 ipcRenderer.send('message', 'Hello from App.vue!');
+window.io.readFile('asitwas.mp3').then((r) => console.log('From Renderer process: ', r))
 
 </script>
 
