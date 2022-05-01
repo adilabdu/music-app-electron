57853261afbe44537a3cc7b2f04e2b1a7b266faf
adilabdu <adilabdu68@gmail.com>
2022-05-02 00:16:33 +0300

    Switched playlist order

--- src/renderer/pages/listen.vue
+++ src/renderer/pages/listen.vue
@@ -3,9 +3,9 @@
   <ContentPage page-title="Listen Now">
 
     <DeezerPlaylist :playlistID="'658600625'" :title="'New Releases'" />
+    <DeezerPlaylist :playlistID="'2098157264'" :title="'Hits of the Moment'" />
     <DeezerPlaylist :playlistID="'7662551722'" :title="'2010\'s Raps'" />
     <DeezerPlaylist :playlistID="'1677006641'" :title="'Hip-Hop Hits'" />
-    <DeezerPlaylist :playlistID="'2098157264'" :title="'Hits of the Moment'" />
 
   </ContentPage>
 
