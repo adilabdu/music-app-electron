c0b718b02e281a21a0c9990f88554cd7507a6499
adilabdu <adilabdu68@gmail.com>
2022-05-02 00:04:39 +0300

    New icons for AlbumCarousel pagination

---
+++ src/renderer/components/Icons/next.vue
@@ -0,0 +1,33 @@
+<template>
+
+  <svg :class="styles" :width="width" :height="height" viewBox="0 0 312 1512" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M6 1506L306 756L6 6" :stroke="stroke['color']" :stroke-width="stroke['width']" stroke-linecap="round"/>
+  </svg>
+
+</template>
+
+<script setup>
+
+import { computed } from "vue"
+
+const props = defineProps({
+  styles: {
+    type: String,
+    default: ''
+  },
+  width: {
+    type: Number,
+    default: 24
+  },
+  stroke: {
+    type: Object
+  }
+})
+
+const height = computed(() => props.width * (1512 / 312))
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/renderer/components/Icons/prev.vue
@@ -0,0 +1,33 @@
+<template>
+
+  <svg :class="styles" :width="width" :height="height" viewBox="0 0 311 1511" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M305.5 5.5L5.5 755.5L305.5 1505.5" :stroke="stroke['color']" :stroke-width="stroke['width']" stroke-linecap="round"/>
+  </svg>
+
+</template>
+
+<script setup>
+
+import { computed } from "vue"
+
+const props = defineProps({
+  styles: {
+    type: String,
+    default: ''
+  },
+  width: {
+    type: Number,
+    default: 24
+  },
+  stroke: {
+    type: Object
+  }
+})
+
+const height = computed(() => props.width * (1511 / 311))
+
+</script>
+
+<style scoped>
+
+</style>
