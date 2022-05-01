e2633522d7db02f83ecb42180569ac8393efe1e3
adilabdu <adilabdu68@gmail.com>
2022-05-01 23:59:14 +0300

    Removed test console log

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -5,7 +5,6 @@ import Layout from './layouts/default.vue'
 import { ipcRenderer } from './electron'
 
 ipcRenderer.send('message', 'Hello from App.vue!');
-window.io.readFile('asitwas.mp3').then((r) => console.log('From Renderer process: ', r))
 
 </script>
 
