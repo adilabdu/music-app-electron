a26757154db474d46915990f30e9b0708ed27a60
adilabdu <adilabdu68@gmail.com>
2022-05-03 23:57:54 +0900

    New component, view, icon for functional 'buttons' displayed within AlbumCarousel

---
+++ src/renderer/views/AddLocationFunction.vue
@@ -0,0 +1,30 @@
+<template>
+
+  <FunctionCard @clicked="getDirectory" class="function-card" :function-info="{ name: 'Add Location', description: 'Add a local location to populate your music library.' }">
+
+    <template #icon>
+      <PlusIcon class="function-icon" :width="48" :class="'fill-[#818181]'" />
+    </template>
+
+  </FunctionCard>
+
+</template>
+
+<script setup>
+
+  import FunctionCard from "../components/FunctionCard.vue";
+  import PlusIcon from "../components/Icons/plus.vue";
+
+  function getDirectory() {
+    
+  }
+
+</script>
+
+<style scoped>
+
+  .function-card:hover .function-icon {
+    @apply fill-[#4D4D4D] transition duration-300;
+  }
+
+</style>
