339599d488449ad864d5731c7daddec19b2f09b9
adilabdu <adilabdu68@gmail.com>
2022-05-06 00:01:00 +0300

    New pages structure

---
+++ src/renderer/pages/albums/all.vue
@@ -0,0 +1,40 @@
+<template>
+
+  <ContentPage page-title="Albums">
+
+    <AlbumCarousel :display="24" :loading="loading">
+
+      <AlbumCard v-for="album in albums" :album-info="{ id: album.id, title: album.title, artist: album.artist.name, artwork: album.artwork, tracks: null, }"  />
+
+    </AlbumCarousel>
+
+  </ContentPage>
+
+</template>
+
+<script setup>
+
+  import { onMounted, ref } from "vue";
+
+  import ContentPage from "../../components/ContentPage.vue";
+  import AlbumCarousel from "../../components/AlbumCarousel.vue";
+  import AlbumCard from "../../components/AlbumCard.vue";
+
+  const albums = ref()
+  const loading = ref(true)
+
+  onMounted(() => {
+
+    albums.value = window.api.Album.all()
+    loading.value = false
+    console.log("All albums:", albums.value)
+    console.log("Tracklist:", window.api.Track.tracklist(2))
+
+  })
+
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/renderer/pages/albums/single.vue
@@ -0,0 +1,19 @@
+<template>
+
+  <Tracklist :album="album" />
+
+</template>
+
+<script setup>
+
+  import { computed } from "vue"
+  import store from "../../store/index"
+  import Tracklist from "../../views/Tracklist.vue";
+
+  const album = computed(() => store.state.album.currentAlbum)
+
+</script>
+
+<style scoped>
+
+</style>
