cd6930febeeaca151c7c4098e586b9ffa05da015
adilabdu <adilabdu68@gmail.com>
2022-05-05 23:59:58 +0300

    props attribute changed

--- src/renderer/components/ContentPage.vue
+++ src/renderer/components/ContentPage.vue
@@ -1,9 +1,9 @@
 <template>
 
-  <main class="min-h-screen grow w-full text-white max-w-[1600px]" style="padding: 24px 0 0 0;">
+  <main class="min-h-screen grow w-full text-white max-w-[1600px]" style="margin: 40px 0 0 0;">
 
-    <h1 class="text-[34px] text-[#EFEFEF] font-semibold capitalize mb-[6px] px-[40px]">{{ pageTitle }}</h1>
-    <div class="px-[40px]">
+    <h1 v-if="!! pageTitle" class="text-[34px] text-[#EFEFEF] font-semibold capitalize mb-[6px] px-[40px]">{{ pageTitle }}</h1>
+    <div v-if="!! pageTitle" class="px-[40px]">
       <hr class="border-[#3B3B3B] border-t-[1px]">
     </div>
 
