0a405b55beeefc80dcc50c3718e63c11d778541b
adilabdu <adilabdu68@gmail.com>
2022-05-05 23:59:17 +0300

    Renaming props, Adding click function on card

--- src/renderer/components/AlbumCard.vue
+++ src/renderer/components/AlbumCard.vue
@@ -1,10 +1,10 @@
 <template>
 
   <article class="flex flex-col min-w-[144px]">
-    <div class="relative">
+    <div @click="goToTracklist" class="relative">
       <div class="flex justify-between items-end group hover:bg-opacity-20 absolute w-full h-full bg-black bg-opacity-0 rounded-md transition duration-150">
-        <div v-if="!trackInfo.track" class="flex items-center justify-center m-2 w-8 h-8 rounded-full bg-white bg-opacity-0" />
-        <div v-if="playMe() && trackInfo.track" @click.prevent="startPlaying" class="flex items-center justify-center m-2 w-8 h-8 rounded-full bg-white group-hover:bg-opacity-40 bg-opacity-0 hover:!bg-opacity-100 hover:bg-[#FF8400]">
+        <div v-if="!albumInfo.track" class="flex items-center justify-center m-2 w-8 h-8 rounded-full bg-white bg-opacity-0" />
+        <div v-if="playMe() && albumInfo.track" @click.prevent="startPlaying" class="flex items-center justify-center m-2 w-8 h-8 rounded-full bg-white group-hover:bg-opacity-40 bg-opacity-0 hover:!bg-opacity-100 hover:bg-[#FF8400]">
           <PlayIcon class="ml-[0.1rem] w-full aspect-auto scale-[45%] fill-white group-hover:opacity-100 opacity-0" />
         </div>
         <div v-if="pauseMe()" @click.prevent="pausePlaying" class="flex items-center justify-center m-2 w-8 h-8 rounded-full bg-white group-hover:bg-opacity-40 bg-opacity-0 hover:!bg-opacity-100 hover:bg-[#FF8400]">
@@ -19,13 +19,13 @@
         </div>
       </div>
       <img
-          :src="trackInfo['artwork']"
+          :src="albumInfo['artwork']"
           class="rounded-md w-full aspect-square object-cover"
       />
     </div>
     <div class="pt-[6px] flex flex-col">
-      <a href="#" class="hover:underline cursor-pointer text-[12px] text-[#EFEFEF]">{{ trackInfo['title'] }}</a>
-      <a href="#" class="hover:underline cursor-pointer text-[12px] text-[#EFEFEF] opacity-75 leading-none truncate">{{ trackInfo['artist'] }}</a>
+      <a href="#" class="hover:underline cursor-pointer text-[12px] text-[#EFEFEF]">{{ albumInfo['title'] }}</a>
+      <a href="#" class="hover:underline cursor-pointer text-[12px] text-[#EFEFEF] opacity-75 leading-none truncate">{{ albumInfo['artist'] }}</a>
     </div>
   </article>
 
@@ -36,6 +36,7 @@
   import { onMounted, computed } from "vue"
 
   import store from "../store"
+  import router from "../router"
 
   import PlayIcon from "./Icons/play.vue"
   import PauseIcon from "./Icons/pause.vue"
@@ -47,7 +48,7 @@
       PauseIcon
     },
     props: {
-      trackInfo: {
+      albumInfo: {
         required: true,
         type: Object
       }
@@ -58,7 +59,7 @@
       const loadedTrack = computed(() => store.state.player.currentTrack.track)
 
       function startPlaying() {
-        store.dispatch('loadTrack', props.trackInfo)
+        store.dispatch('loadTrack', props.albumInfo)
         store.dispatch('play')
       }
 
@@ -67,14 +68,21 @@
       }
 
       function playMe() {
-        return !playing.value || (playing.value && (loadedTrack.value !== props.trackInfo.track))
+        return !playing.value || (playing.value && (loadedTrack.value !== props.albumInfo.track))
       }
 
       function pauseMe() {
-        return playing.value && (loadedTrack.value === props.trackInfo.track)
+        return playing.value && (loadedTrack.value === props.albumInfo.track)
+      }
+      
+      function goToTracklist() {
+
+        store.dispatch('setCurrentAlbum', props.albumInfo)
+        router.push({ path: `/albums/${props.albumInfo.id}` })
+
       }
 
-      return { startPlaying, pausePlaying, playMe, pauseMe }
+      return { startPlaying, pausePlaying, playMe, pauseMe, goToTracklist }
 
     }
   }
