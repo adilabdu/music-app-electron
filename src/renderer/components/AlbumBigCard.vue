24e75e727b39aaa5193dd5590a121716fda64681
adilabdu <adilabdu68@gmail.com>
2022-05-06 22:43:37 +0300

    New components to build the Tracklist view

---
+++ src/renderer/components/AlbumBigCard.vue
@@ -0,0 +1,61 @@
+<template>
+
+  <div class="flex w-full h-[270px] justify-start px-[40px] px-[40px]">
+
+    <div class="w-[270px]">
+      <img alt="Album Artwork" :src="!! album.artwork ? album.artwork : ''" class="w-full aspect-square rounded-md" />
+    </div>
+
+    <div class="grow flex flex-col justify-between items-start pl-[34px] py-[5px]">
+
+      <div class="mt-[79.5px] flex flex-col justify-end items-start gap-2">
+
+        <h1 class="font-medium text-[26px] text-[#FFFFFFEB] leading-none"> {{ !! album.title ? album.title : '' }} </h1>
+        <h1 class="font-light text-[26px] text-[#FF8400] leading-none"> {{ !! album.artist ? album.artist : '' }} </h1>
+        <h3 class="text-[12px] text-[#FFFFFFA3] leading-none font-medium uppercase">R&B / Soul &#8226;	2018</h3>
+
+      </div>
+
+      <div class="flex w-full justify-between">
+
+        <div class="buttons flex gap-4">
+          <button class="h-[28px] rounded-md bg-[#FF8400] px-[28px] text-[13px] font-medium flex items-center justify-center gap-2">
+            <PlayIcon :width="10" :class="'fill-[#FFFFFF]'" />
+            Play
+          </button>
+          <button class="h-[28px] rounded-md bg-[#FF8400] px-[28px] text-[13px] font-medium flex items-center justify-center gap-2">
+            <PlayIcon :width="10" :class="'fill-[#FFFFFF]'" />
+            Shuffle
+          </button>
+        </div>
+
+        <button class="h-full aspect-square rounded-full bg-[#FF8400] bg-opacity-10 flex gap-0.5 items-center justify-center">
+          <div class="w-[0.2rem] h-[0.2rem] rounded-full bg-[#FF8400]" />
+          <div class="w-[0.2rem] h-[0.2rem] rounded-full bg-[#FF8400]" />
+          <div class="w-[0.2rem] h-[0.2rem] rounded-full bg-[#FF8400]" />
+        </button>
+
+      </div>
+
+    </div>
+
+  </div>
+
+</template>
+
+<script setup>
+
+  import PlayIcon from "./Icons/play.vue";
+
+  const props = defineProps({
+    album: {
+      type: Object,
+      required: true
+    }
+  })
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/renderer/components/Table.vue
@@ -0,0 +1,63 @@
+<template>
+
+  <div v-if="!loading" class="tracklist-table flex flex-col w-full min-h-[36px] px-[40px] my-[40px]">
+
+    <div v-for="track in data" class="group hover:!bg-[#353535] track w-full rounded-md flex items-center pl-[7px] pr-[18px] text-[13px] font-light h-[46px]">
+
+      <div class="-translate-x-[125%] h-[40px] w-8 opacity-0 hover:opacity-100 absolute flex items-center justify-center">
+        <HeartIcon :width="8" :stroke="{ color: '#FF8400', width: 30 }" :class="''" />
+      </div>
+
+      <div class="relative flex items-center">
+        <PlayingIcon :class="[(5 === 6) ? 'opacity-100' : 'opacity-0']" class="absolute ml-1" />
+        <p :class="[(5 === 5) ? '!opacity-100' : '!opacity-0']" class="max-w-fit pl-2 pr-6 text-center text-[#FFFFFFA3] group-hover:opacity-0 opacity-100">
+          {{ track[0] }}</p>
+      </div>
+      <p class="min-w-[50%] text-[#FFFFFFEB]">{{ track[1] }}</p>
+      <p class="grow truncate text-[#FFFFFFA3]">{{ track[2] }}</p>
+      <div class="w-[42px] flex items-center justify-center opacity-0 group-hover:opacity-100">
+        <PlusIcon :width="16" :class="'fill-[#FF8400]'" />
+      </div>
+      <p class="text-[#FFFFFFA3] max-w-fit text-right pr-1">{{ track[3] }}</p>
+      <div class="w-[28px] flex items-center justify-center">
+        <button class="flex gap-0.5 items-center justify-center">
+          <div class="w-[0.2rem] h-[0.2rem] rounded-full bg-[#FF8400]" />
+          <div class="w-[0.2rem] h-[0.2rem] rounded-full bg-[#FF8400]" />
+          <div class="w-[0.2rem] h-[0.2rem] rounded-full bg-[#FF8400]" />
+        </button>
+      </div>
+
+    </div>
+
+  </div>
+
+</template>
+
+<script setup>
+
+  import PlayIcon from "../components/Icons/play.vue";
+  import PlayingIcon from "./Icons/playing.vue";
+  import PlusIcon from "./Icons/plus.vue";
+  import HeartIcon from "./Icons/heart.vue";
+
+  // TODO: props to take in data to render into table
+  const props = defineProps({
+    data: {
+      type: Object,
+      required: true
+    },
+    loading: {
+      type: Boolean,
+      default: false
+    }
+  })
+
+</script>
+
+<style scoped>
+
+  .tracklist-table .track:nth-child(odd) {
+    @apply bg-[#252525];
+  }
+
+</style>
