60377c150b2233b7e8bf79c34253931a94250325
adilabdu <adilabdu68@gmail.com>
2022-05-06 22:49:44 +0300

    New 'play' animation added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
         "./src/**/*.{vue,js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            animation: {
+                play: 'play 500ms linear infinite alternate'
+            },
+            keyframes: {
+                play: {
+                    '0%': { height: '0.125rem' },
+                    '25%': { height: '0.25rem' },
+                    '50%': { height: '0.5rem' },
+                    '75%': { height: '0.75rem' },
+                    '100%': { height: '1rem' }
+                }
+            }
+        },
     },
     plugins: [],
 }
