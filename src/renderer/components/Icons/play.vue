da7b2e181f12bbfe80e56ca65593ac110f97cd1b
adilabdu <adilabdu68@gmail.com>
2022-05-06 22:47:26 +0300

    Added 'cursor-pointer' CSS class to icons

--- src/renderer/components/Icons/play.vue
+++ src/renderer/components/Icons/play.vue
@@ -1,6 +1,6 @@
 <template>
 
-  <svg :class="styles" :width="width" :height="height" viewBox="0 0 70 80" xmlns="http://www.w3.org/2000/svg">
+  <svg class="cursor-pointer" :class="styles" :width="width" :height="height" viewBox="0 0 70 80" xmlns="http://www.w3.org/2000/svg">
     <path d="M67.5 35.6699C70.8333 37.5944 70.8333 42.4056 67.5 44.3301L7.5 78.9711C4.16667 80.8956 0 78.49 0 74.641L0 5.35899C0 1.50998 4.16667 -0.895644 7.5 1.02886L67.5 35.6699Z"/>
   </svg>
 
--- src/renderer/components/Icons/plus.vue
+++ src/renderer/components/Icons/plus.vue
@@ -1,6 +1,6 @@
 <template>
 
-  <svg :class="styles" :width="width" :height="height" viewBox="0 0 300 300" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg class="cursor-pointer" :class="styles" :width="width" :height="height" viewBox="0 0 300 300" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M300 150C300 232.843 232.843 300 150 300C67.1573 300 0 232.843 0 150C0 67.1573 67.1573 0 150 0C232.843 0 300 67.1573 300 150ZM25.8729 150C25.8729 218.554 81.4465 274.127 150 274.127C218.554 274.127 274.127 218.554 274.127 150C274.127 81.4465 218.554 25.8729 150 25.8729C81.4465 25.8729 25.8729 81.4465 25.8729 150Z"/>
     <rect x="137" y="75" width="25" height="85" rx="12.5"/>
     <rect x="140" y="160" width="25" height="85" rx="12.5" transform="rotate(-90 140 160)"/>
