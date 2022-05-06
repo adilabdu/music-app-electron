6ac8dd3197e3c9781564de131189e6ec0e531b17
adilabdu <adilabdu68@gmail.com>
2022-05-06 22:47:55 +0300

    New animated icon, renders on Tracklist table if track is playing

---
+++ src/renderer/components/Icons/playing.vue
@@ -0,0 +1,25 @@
+<template>
+
+  <div class="flex gap-px rotate-180 h-4">
+    <div :class="animation[0]" class=" w-[0.185rem] bg-[#FF8400]"></div>
+    <div :class="animation[1]" class=" w-[0.185rem] bg-[#FF8400]"></div>
+    <div :class="animation[2]" class=" w-[0.185rem] bg-[#FF8400]"></div>
+    <div :class="animation[3]" class=" w-[0.185rem] bg-[#FF8400]"></div>
+  </div>
+</template>
+
+<script setup>
+
+  // TODO: can mount the animation class after shuffle
+  const animation = [
+      'animate-[play_500ms_200ms_linear_infinite_alternate]',
+      'animate-[play_500ms_500ms_linear_infinite_alternate]',
+      'animate-[play_500ms_100ms_linear_infinite_alternate]',
+      'animate-[play_500ms_400ms_linear_infinite_alternate]'
+  ]
+
+</script>
+
+<style scoped>
+
+</style>
