a889ea656cad0d9e9316441c4937cbf4fa7e1a31
adilabdu <adilabdu68@gmail.com>
2022-05-08 17:13:09 +0300

    Changed return type of readDirectory function

---
+++ src/main/fs/manager.js
@@ -0,0 +1,81 @@
+const { promises: fs } = require('fs')
+const path = require('path')
+
+const dbManager = require('../database/manager')
+
+export const readDirectory = async () => {
+
+    const fileNames = []
+    const folders = []
+
+    const content = await fs.readdir(dbManager.config()['library_location'], { withFileTypes: true })
+
+    try {
+        fileNames.push(content.filter( file => path.extname(file.name) === '.mp3' || path.extname(file.name) === '.m4a' ))
+        folders.push(content.filter(file => !file.isFile()))
+    } catch(error) {
+        console.log(error)
+    }
+
+    return {
+        fileNames: fileNames[0].map(item => item.name),
+        folders: folders[0].map(item => item.name)
+    }
+
+}
+// TODO: needs fixing
+export const readFilesRecursively = async (currentFolder, folders, fileNames) => {
+
+    console.log('Inside function')
+
+    const index = folders.indexOf(currentFolder);
+    if (index > -1) {
+        folders.splice(index, 1);
+    }
+
+    const content = await fs.readdir(currentFolder, { withFileTypes: true })
+
+    try {
+        fileNames.push( content.filter(file => path.extname(file.name) === '.mp3').map(item => item.name) )
+        folders.push( content.filter(file => !file.isFile()).map(item => currentFolder + item.name) )
+
+        console.log('inside try block:', {
+            fileNames,
+            folders: folders[0]
+        })
+
+    } catch(error) {
+        console.log('Error inside recursive try block:', error)
+    }
+
+    console.log('Passing these values to next call:', {
+        currentFolder: folders[0][0],
+        folders: folders[0],
+        fileNames: fileNames[0]
+    })
+
+    console.log("Checking folders length:", folders)
+    if(folders.length === 0) {
+        return {
+            fileNames,
+            folders,
+        }
+    }
+
+    await readFilesRecursively(folders[0][0], folders[0], fileNames[0])
+
+}
+
+export const readFile = async (fileName) => {
+
+    const file = {}
+
+    try {
+        file.buffer = await fs.readFile('/Users/adilabdu/Documents/music/' + fileName)
+    } catch (error) {
+        console.log(error)
+    }
+
+    return file
+
+}
