1dd15166bf3b67606ddd680d201ed9a898586a82
adilabdu <adilabdu68@gmail.com>
2022-05-09 00:29:26 +0300

    Removed test functions

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,29 +1,9 @@
 <script setup>
 
-  import { ref } from "vue";
-
   import Layout from './layouts/default.vue'
 
   import { ipcRenderer } from './electron'
   ipcRenderer.send('message', 'Hello from App.vue!');
-
-  const fileNames = ref()
-
-  window.io.readDirectory().then((result) => {
-    fileNames.value = result.fileNames
-  }).then(() => {
-    getFileBuffer()
-  })
-
-  function getFileBuffer() {
-    fileNames.value.forEach((file, index) => {
-      window.io.readFile(file).then((res) => {
-        console.log(`File #${(index+1)}:`, {
-          file: res
-        })
-      })
-    })
-  }
 
 </script>
 
