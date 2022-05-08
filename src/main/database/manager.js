3cedbf983e92b2b5b62b3e3d530aadf8934eb04f
adilabdu <adilabdu68@gmail.com>
2022-05-08 17:14:25 +0300

    New config function to return configuration settings

--- src/main/database/manager.js
+++ src/main/database/manager.js
@@ -1,6 +1,7 @@
 const sqlite = require('better-sqlite3-with-prebuilds')
-const createQuery = require(__dirname + '/create_db.js')
 const database = new sqlite('./orange.db')
+
+const createQuery = require(__dirname + '/create_db.js')
 
 exports.initialize = () => {
     try {
@@ -8,6 +9,27 @@ exports.initialize = () => {
     } catch (e) {
         console.log('CREATE database error: ', e)
     }
+}
+
+exports.config = () => {
+
+    const query = `SELECT * from config`;
+
+    try {
+        const result = database.prepare(query).all()[0]
+        console.log("SELECT Success:", {
+            result,
+            query
+        })
+        return result
+    } catch (e) {
+        console.log("Error during SELECT:", {
+            e,
+            query
+        })
+    }
+
+    return 0
 }
 
 exports.database = database
