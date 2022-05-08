c8ba7321e257de5355de30380a6d6fd0a3343f7e
adilabdu <adilabdu68@gmail.com>
2022-05-08 17:05:57 +0300

    Playing function added to Table rows, Icons appearance set based on state

--- src/renderer/components/Table.vue
+++ src/renderer/components/Table.vue
@@ -2,15 +2,17 @@
 
   <div v-if="!loading" class="tracklist-table flex flex-col w-full min-h-[36px] px-[40px] my-[40px]">
 
-    <div v-for="track in data" class="group hover:!bg-[#353535] track w-full rounded-md flex items-center pl-[7px] pr-[18px] text-[13px] font-light h-[46px]">
+    <div v-for="(track, index) in data" class="group hover:!bg-[#353535] track w-full rounded-md flex items-center pl-[7px] pr-[18px] text-[13px] font-light h-[46px]">
 
       <div class="-translate-x-[125%] h-[40px] w-8 opacity-0 hover:opacity-100 absolute flex items-center justify-center">
         <HeartIcon :width="8" :stroke="{ color: '#FF8400', width: 30 }" :class="''" />
       </div>
 
-      <div class="relative flex items-center">
-        <PlayingIcon :class="[(5 === 6) ? 'opacity-100' : 'opacity-0']" class="absolute ml-1" />
-        <p :class="[(5 === 5) ? '!opacity-100' : '!opacity-0']" class="max-w-fit pl-2 pr-6 text-center text-[#FFFFFFA3] group-hover:opacity-0 opacity-100">
+      <div class="relative flex items-center icons">
+        <PlayIcon v-if="( (!isCurrentlyPlaying(index, track[4])) )" @click="startPlaying(index)" :width="12" :class="'z-30 absolute ml-2 fill-[#FF8400] opacity-0 group-hover:opacity-100'" />
+        <PauseIcon v-if="isPlaying && isCurrentlyPlaying(index, track[4])" @click="pausePlaying" :width="12" class="pause-icon" :class="'z-30 absolute ml-2 fill-[#FF8400] opacity-0'" />
+        <PlayingIcon :class="[ ( isPlaying && isCurrentlyPlaying(index, track[4]) ) ? 'opacity-100' : 'opacity-0']" class="playing-icon z-20 absolute ml-2" />
+        <p :class="[!isCurrentlyPlaying(index, track[4]) ? 'opacity-100' : 'opacity-0']" class="max-w-fit pl-2 pr-6 text-center text-[#FFFFFFA3] group-hover:opacity-0 opacity-100">
           {{ track[0] }}</p>
       </div>
       <p class="min-w-[50%] text-[#FFFFFFEB]">{{ track[1] }}</p>
@@ -35,10 +37,14 @@
 
 <script setup>
 
+  import { computed } from "vue";
+
   import PlayIcon from "../components/Icons/play.vue";
+  import PauseIcon from "../components/Icons/pause.vue";
   import PlayingIcon from "./Icons/playing.vue";
   import PlusIcon from "./Icons/plus.vue";
   import HeartIcon from "./Icons/heart.vue";
+  import store from "../store";
 
   // TODO: props to take in data to render into table
   const props = defineProps({
@@ -52,12 +58,50 @@
     }
   })
 
+  function startPlaying(index) {
+
+    if(!! !store.state.player.currentTrack ||
+        (store.state.player.currentTrack.location !==  props.data[index][4])) {
+
+      store.dispatch('unloadTrack')
+      store.dispatch('loadTrack', {
+        title: store.state.album.currentAlbum.tracklist[index].title,
+        artist: store.state.album.currentAlbum.artist,
+        album: store.state.album.currentAlbum.title,
+        location: store.state.album.currentAlbum.tracklist[index].location,
+        artwork: store.state.album.currentAlbum.artwork
+      })
+    }
+
+    store.dispatch('play')
+  }
+
+  function pausePlaying() {
+    store.dispatch('pause')
+  }
+
+  // TODO: also check which row of `data` corresponds with currently `loadedTrack`
+  const isPlaying = computed(() => store.state.player.playing)
+
+  const isCurrentlyPlaying = (index, trackLocation) => {
+    console.log('isPlayingThisTrack:', store.state.album.currentAlbum.tracklist[index].location === trackLocation)
+    return (store.state.player.currentTrack.location === trackLocation)
+  }
+
 </script>
 
 <style scoped>
 
   .tracklist-table .track:nth-child(odd) {
     @apply bg-[#252525];
+  }
+
+  .icons:hover .playing-icon {
+    @apply opacity-0;
+  }
+
+  .icons:hover .pause-icon {
+    @apply opacity-100;
   }
 
 </style>
