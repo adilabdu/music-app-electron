ce0494c0fb8c496737b1de80d1b71924148b28a4
adilabdu <adilabdu68@gmail.com>
2022-05-12 00:40:51 +0300

    Dynamic rendering of DeezerPlaylist components, handling loading state of each and passing to ContentPage

--- src/renderer/pages/listen.vue
+++ src/renderer/pages/listen.vue
@@ -1,11 +1,8 @@
 <template>
 
-  <ContentPage page-title="Listen Now">
+  <ContentPage :loading="pageLoading" page-title="Listen Now">
 
-    <DeezerPlaylist :playlistID="'658600625'" :title="'New Releases'" />
-    <DeezerPlaylist :playlistID="'2098157264'" :title="'Hits of the Moment'" />
-    <DeezerPlaylist :playlistID="'7662551722'" :title="'2010\'s Raps'" />
-    <DeezerPlaylist :playlistID="'1677006641'" :title="'Hip-Hop Hits'" />
+    <DeezerPlaylist v-for="playlist in playlists" @loading="loading" :playlistID="playlist.id" :title="playlist.title" />
 
   </ContentPage>
 
@@ -13,8 +10,44 @@
 
 <script setup>
 
+  import { ref, computed } from "vue";
+
   import ContentPage from '../components/ContentPage.vue';
   import DeezerPlaylist from "../views/DeezerPlaylist.vue"
+
+  const playlists = [
+    {
+      id: '658600625',
+      title: 'New Releases'
+    },
+    {
+      id: '2098157264',
+      title: 'Hits of the Moment'
+    },
+    {
+      id: '7662551722',
+      title: '2010\'s Raps'
+    },
+    {
+      id: '1677006641',
+      title: 'Hip-Hop Hits'
+    },
+  ]
+
+  const loadingComponents = ref([])
+  function loading(value) {
+    if(value) {
+      loadingComponents.value.push('loading')
+    } else {
+      loadingComponents.value.pop()
+    }
+
+    console.log(loadingComponents.value)
+  }
+
+  const pageLoading = computed(() => {
+    return (loadingComponents.value.length > 0)
+  })
 
 </script>
 
