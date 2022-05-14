b06c48e788ce685d4c53fd202575d6b4789c9e31
adilabdu <adilabdu68@gmail.com>
2022-05-14 08:10:48 +0300

    New modules modal and menu imported

--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -4,12 +4,14 @@ import playlists from './modules/playlists.js'
 import album from './modules/album'
 import player from './modules/player.js'
 import ui from './modules/ui.js'
+import menu from './modules/menu.js'
 
 export default createStore({
     modules: {
         playlists,
         album,
         player,
-        ui
+        ui,
+        menu
     }
 })
