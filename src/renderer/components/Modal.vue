88a22a9e8fe67245f8a1d5624fecba0bb498159c
adilabdu <adilabdu68@gmail.com>
2022-05-14 08:10:11 +0300

    New Modal component that renders full screen when necessary (eg. to render ContextMenu over it)

---
+++ src/renderer/components/Modal.vue
@@ -0,0 +1,28 @@
+<template>
+
+  <div @click="close" v-if="display" class="z-[60] fixed w-full h-screen">
+
+    <AlbumCardMenu :open="true" />
+
+  </div>
+
+</template>
+
+<script setup>
+
+  import { computed } from "vue"
+  import store from "../store/index"
+
+  import AlbumCardMenu from "../views/AlbumCardMenu.vue"
+
+  const display = computed(() => store.state.ui.modalOpened)
+
+  function close() {
+    store.dispatch('toggleModalView')
+  }
+
+</script>
+
+<style scoped>
+
+</style>
