3cdd7096c526711611bf3bdd5f3a9677c32a11f7
adilabdu <adilabdu68@gmail.com>
2022-05-14 08:11:56 +0300

    New state, actions and mutations to deal with the new Modal component view

--- src/renderer/store/modules/ui.js
+++ src/renderer/store/modules/ui.js
@@ -1,16 +1,23 @@
 const state = {
-    queueListOpened: false
+    queueListOpened: false,
+    modalOpened: false
 }
 
 const actions = {
     toggleQueueList(context) {
         context.commit('toggleQueueList')
+    },
+    toggleModalView(context) {
+        context.commit('toggleModalView')
     }
 }
 
 const mutations = {
     toggleQueueList(state) {
         state.queueListOpened = !state.queueListOpened
+    },
+    toggleModalView(state) {
+        state.modalOpened = !state.modalOpened
     }
 }
 
