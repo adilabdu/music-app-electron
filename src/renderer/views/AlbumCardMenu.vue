c26e2e6adc9f1ba9fae5f904b24704789641617a
adilabdu <adilabdu68@gmail.com>
2022-05-15 13:15:09 +0900

    ContextMenu's passed AlbumCard position with the new style prop to render the ContextMenu in the correct location in the Modal

--- src/renderer/views/AlbumCardMenu.vue
+++ src/renderer/views/AlbumCardMenu.vue
@@ -1,14 +1,19 @@
 <template>
 
-  <ContextMenu class="" :items="menuItems" :open="open" />
-
-  <ContextMenu v-if="!loading" :position="'translate-x-full'" :items="addToPlaylistMenuItems" :open="open" />
+    <ContextMenu :style="{ transform: 'translateY('+ 0 +'%)', top: (renderPosition.y - menuItems.length*34) + 'px', left: (renderPosition.x-200) + 'px' }" @hovered="menuHovered" class="" :items="menuItems" :open="open" />
+    <ContextMenu
+        @leftComponent="playlistMenuLeft"
+        v-if="!loading && playlistMenuOpened==='Add to Playlist'"
+        :style="{ transform: 'translateX(100%)', top: (renderPosition.y - menuItems.length*34) + 'px', left: (renderPosition.x-200) + 'px' }"
+        :items="addToPlaylistMenuItems"
+        :open="open" />
 
 </template>
 
 <script setup>
 
-  import { onMounted, ref } from "vue";
+  import {onMounted, ref, computed, render} from "vue";
+  import store from "../store/index"
   import ContextMenu from "../components/ContextMenu.vue"
 
   const emit = defineEmits(['close'])
@@ -49,6 +54,26 @@
       }
     },
     {
+      title: 'Play Next',
+      icon: {
+        name: 'TrackIcon',
+        props: {
+          width: 14,
+          class: 'fill-[#FFFFFFA3]'
+        }
+      }
+    },
+    {
+      title: 'Play Later',
+      icon: {
+        name: 'TrackIcon',
+        props: {
+          width: 14,
+          class: 'fill-[#FFFFFFA3]'
+        }
+      }
+    },
+    {
       title: 'Love',
       icon: {
         name: 'HeartIcon',
@@ -83,9 +108,26 @@
     },
   ]
 
+  const playlistMenuOpened = ref(null)
+  function menuHovered(component) {
+    playlistMenuOpened.value = component
+  }
+
+  function playlistMenuLeft() {
+    playlistMenuOpened.value = null
+  }
+
   function close() {
     emit('close')
   }
+
+  const renderPosition = computed(() => store.state.menu.contextMenuPosition)
+  // const menuPosition = computed(() => {
+  //   let position = []
+  //   if ( (renderPosition.value.width - renderPosition.value.x) < 450 ) {
+  //     position.push('')
+  //   }
+  // })
 
 </script>
 
