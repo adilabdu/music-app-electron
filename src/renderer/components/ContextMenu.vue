3825c2f9ef51fb368a319cfdb69470588e6b6843
adilabdu <adilabdu68@gmail.com>
2022-05-15 13:12:07 +0900

    New style props to get css style objects, 'left-component' emit to trigger ContextMenu mouseeleave

--- src/renderer/components/ContextMenu.vue
+++ src/renderer/components/ContextMenu.vue
@@ -1,16 +1,16 @@
 <template>
 
-  <div id="menu" v-if="open"
-       :class="position"
+  <div @mouseleave="leftMenu" id="menu" v-if="open"
+       :style="style"
        class="outline outline-[1.5px] outline-offset-[-2px] outline-white/[0.35] flex flex-col
        justify-center items-center absolute w-[200px] backdrop-blur-md bg-[#202020] bg-opacity-[0.50]
        rounded-md z-50">
 
     <ul class="w-[196px] divide-y divide-white/[0.35]">
       <ContextMenuItem
-          @mouseover="hovered(item.icon.name)"
-          @mouseleave="left(item.icon.name)"
-          @clicked="clicked(item.icon.name)"
+          @mouseover="hovered(item.title)"
+          @mouseleave="left(item.title)"
+          @clicked="clicked(item.title)"
           v-for="item in items"
           :title="item.title"
           :iconComponent="item.icon"
@@ -36,12 +36,12 @@
       type: Array,
       default: []
     },
-    position: {
-      type: String,
-      default: ''
+    style: {
+      type: Object,
+      default: {}
     }
   })
-  const emit = defineEmits(['close', 'clicked', 'hovered', 'left'])
+  const emit = defineEmits(['close', 'clicked', 'hovered', 'left', 'left-component'])
 
   const opened = ref(true)
   function close() {
@@ -58,6 +58,10 @@
 
   function left(component) {
     emit('left', component)
+  }
+
+  function leftMenu() {
+    emit('left-component')
   }
 
   if(props.position !== '') {
