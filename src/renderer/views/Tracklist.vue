2c1494297ef3e1884f76a9397f9cde99f2bcc35e
adilabdu <adilabdu68@gmail.com>
2022-05-23 19:00:38 +0900

    Make tracklist a computed data

--- src/renderer/views/Tracklist.vue
+++ src/renderer/views/Tracklist.vue
@@ -11,6 +11,8 @@
 
 <script setup>
 
+  import { computed } from "vue"
+
   import ContentPage from "../components/ContentPage.vue";
   import AlbumBigCard from "../components/AlbumBigCard.vue";
   import Table from "../components/Table.vue";
@@ -22,15 +24,16 @@
     }
   })
 
-  const tracklist = props.album['tracklist'].map((track) => {
+  const tracklist = computed(() => props.album.tracklist.map((track, index) => {
     return {
       0: track['track_position'],
       1: track.title,
       2: (track.artist === props.album.artist) ? '' : track.artist,
       3: track.duration,
-      4: track.location
+      4: track.location,
+      5: index
     }
-  })
+  }))
 
 </script>
 
