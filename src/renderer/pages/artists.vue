1836f76bff5da3afa3eff79202a69fd119c91fc2
adilabdu <adilabdu68@gmail.com>
2022-05-25 18:44:25 +0300

    Call the newly exposed populateDatabase function when FunctionCard is clicked

--- src/renderer/pages/artists.vue
+++ src/renderer/pages/artists.vue
@@ -1,21 +1,27 @@
 <template>
 
-  <ContentPage page-title="Artists" />
+  <ContentPage page-title="Artists">
+
+    <FTable :data="data">
+      <FRow :show="'firstName'" />
+    </FTable>
+
+  </ContentPage>
 
 </template>
 
-<script>
+<script setup>
 
   import ContentPage from "../components/ContentPage.vue";
+  import FTable from "../test/FTable.vue";
+  import FRow from "../test/FRow.vue"
 
-  export default {
-
-    name: "artists",
-    components: {
-      ContentPage
-    },
-    setup() { }
-  }
+  const data = [
+    { firstName: 'John', lastName: 'Lennon', instrument: 'Guitar', birthday: '04/10/1940', songs: 72 },
+    { firstName: 'Paul', lastName: 'McCartney', instrument: 'Bass', birthday: '18/06/1942', songs: 70 },
+    { firstName: 'George', lastName: 'Harrison', instrument: 'Guitar', birthday: '25/02/1943', songs: 22 },
+    { firstName: 'Ringo', lastName: 'Starr', instrument: 'Drums', birthday: '07/07/1940', songs: 2 },
+  ]
 
 </script>
 
